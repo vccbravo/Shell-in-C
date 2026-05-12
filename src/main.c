de6f4853e6f6770ac36e47e339ab51b825ec3aed
vccbravo <veronicacbravo@gmail.com>
2026-05-12 05:36:16 +0100

    reading lines implemented

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 void lsh_loop(void){
     char *line;
     char **args;
@@ -11,7 +17,46 @@ void lsh_loop(void){
 
         free(line);
         free(status);
-    } while (status);   //coment svsfssd
+    } while (status);
+}
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read(void){
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;  // int because EOF is an int not a char
+
+    if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a nul character and return
+        if (c == EOF || c == '\0') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+        
+        // If we have exceeded  the buffer, reallocate
+        if (position >= bufsize) {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+            
+        }
+        
+    }
     
 }
 
